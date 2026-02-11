8062bd306b99a3983a45365cb0fe67fc1707ff51
Alex Asriian <alexasr00798@gmail.com>
2026-02-12 01:39:44 +0200

    task mod

--- js/task-1.js
+++ js/task-1.js
@@ -19,7 +19,7 @@ const customer = {
     return this.orders;
   },
   addOrder(cost, order) {
-    this.balance = cost - cost * this.discount;
+    this.balance -= cost - cost * this.discount;
     this.orders.push(order);
   },
   // Change code above this line
